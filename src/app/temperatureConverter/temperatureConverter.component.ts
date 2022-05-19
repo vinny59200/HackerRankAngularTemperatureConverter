92e3abbc4e720c643b9cc6f97536bfa954346da0
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2022-05-19 05:16:16 +0200

    init

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'temperature-converter',
@@ -8,9 +8,25 @@ import {Component, Input, OnInit} from '@angular/core';
 
 export class TemperatureConverter implements OnInit {
 
+  fahrenheit: string = '';
+  celsius: string = '';
   ngOnInit() {
     // C = (F − 32) × 5/9
     // F = C*9/5 + 32
+  }
+
+  onCelsiusChange(searchValue: string): void {
+    console.log(searchValue);
+    let string = this.celsius;
+    let num = parseInt(string) * 9 / 5 + 32;
+    this.fahrenheit = num.toString();
+  }
+
+  onFahrenheitChange(searchValue: string): void {
+    console.log(searchValue);
+    let string = this.fahrenheit;
+    let num = (parseInt(string)- 32) * 5 / 9;
+    this.celsius = num.toString();
   }
 
 }
